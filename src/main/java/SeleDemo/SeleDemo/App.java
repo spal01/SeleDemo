588ff2734dad2d59626099c55620fc9df937abae
palsumanta004 <palsumanta04@gmail.com>
2021-06-12 23:32:37 +0530

    1st comit

---
+++ src/main/java/SeleDemo/SeleDemo/App.java
@@ -0,0 +1,13 @@
+package SeleDemo.SeleDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SeleAuto/TestSele.java
@@ -0,0 +1,16 @@
+package SeleAuto;
+
+import org.testng.annotations.Test;
+
+public class TestSele {
+	
+  @Test
+  public void test1() {
+	  System.out.println("test1");
+  }
+  
+  @Test
+  public void test2() {
+	  System.out.println("test2");
+  }
+}
